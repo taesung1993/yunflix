bc67db168bf4bd6c4422b545c724f8fa868aae24
taesung1993 <cheonyulin@gmail.com>
2020-10-24 16:11:35 +0900

    Updates

---
+++ precache-manifest.ff7fd76c6c6a33c453435cf27f1e2c91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a13dfec76ce568820bbf058f96a24879",
+    "url": "/yunflix/index.html"
+  },
+  {
+    "revision": "42c6f6c69c2f9adcdb54",
+    "url": "/yunflix/static/js/2.11be17f6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/yunflix/static/js/2.11be17f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13be8c7ce0acbbc56be6",
+    "url": "/yunflix/static/js/main.3e981ca5.chunk.js"
+  },
+  {
+    "revision": "1e503d8c9123c419d493",
+    "url": "/yunflix/static/js/runtime-main.7e8a81a9.js"
+  }
+]);
