d5451ab2f8c0cb620a71a29ed7d107afd9d0772f
taesung1993 <cheonyulin@gmail.com>
2020-10-24 15:33:06 +0900

    App.js

---
+++ src/Components/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {BrowserRouter as Router} from "react-router-dom";
+import Routes from "Components/Routes";
+import Header from "Components/Header";
+import UserContextProvider from "Context";
+import GlobalStyles from "Components/GlobalStyles";
+
+const App = () => {
+  return (
+    <UserContextProvider>
+      <Router>
+        <Header/>
+        <Routes/>
+      </Router>
+      <GlobalStyles/>
+    </UserContextProvider>
+  );
+}
+
+export default App;
